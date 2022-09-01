f699a6a527e22eca755e210bf2d0c33bad907be4
suhcrates <suhcrates@hanmail.net>
2022-09-01 15:26:29 +0900

    asdf

--- sql_toolbox.py
+++ sql_toolbox.py
@@ -2,7 +2,16 @@ from database import db, cursor
 
 
 def time_checker(what0):
+    config = {
+        'user': 'root',
+        'password': 'Seoseoseo7!',
+        'host': 'localhost',
+        # 'database':'shit',
+        'port': '3306'
+    }
 
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
     cursor.execute(
         f"""
         select date0 from dangbun_stuffs.brods where brod="{what0}"
